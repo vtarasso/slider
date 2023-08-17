8821b388af87f9852e59e6d2b9ef72adc77f20f4
Vitaliy Tarassov <apxustudio@gmail.com>
2023-08-17 20:18:49 +0600

    update: app.js slide with keyboard

--- app.js
+++ app.js
@@ -22,6 +22,15 @@ downBtn.addEventListener('click', () => {
     changeSlide('down')
 })
 
+document.addEventListener('keydown', event => {
+    // console.log(event.key)
+    if (event.key === 'ArrowUp') {
+        changeSlide('up')
+    } else if (event.key === 'ArrowDown') {
+        changeSlide('down')
+    }
+})
+
 function changeSlide(direction) {
     if (direction === 'up') {
         activeSlideIndex++
